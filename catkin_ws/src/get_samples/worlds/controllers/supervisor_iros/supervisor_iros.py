cb7c2c884b9e99c505db196b373f192cc3d0d300
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2024-03-05 20:32:28 -0600

    Modificación de mundo left-2 para agregar coches con movimiento lateral

--- catkin_ws/src/get_samples/worlds/controllers/supervisor_iros/supervisor_iros.py
+++ catkin_ws/src/get_samples/worlds/controllers/supervisor_iros/supervisor_iros.py
@@ -28,6 +28,9 @@ def main():
     
     speed_cars_left_lane = 0.0
     speed_cars_right_lane = 0.0
+
+    lateral_car_1 = robot.getFromDef('lateral_car_1')
+    lateral_car_2 = robot.getFromDef('lateral_car_2')
     
     cars = [robot.getFromDef('vehicle_1'), robot.getFromDef('vehicle_2'),
 robot.getFromDef('vehicle_3'), robot.getFromDef('vehicle_4'),  robot.getFromDef('vehicle_5'), robot.getFromDef('vehicle_6'), robot.getFromDef('vehicle_7'), robot.getFromDef('vehicle_8'), robot.getFromDef('vehicle_9'), robot.getFromDef('vehicle_10')]
@@ -77,7 +80,8 @@ robot.getFromDef('vehicle_3'), robot.getFromDef('vehicle_4'),  robot.getFromDef(
     print("Supervisor.->Start signal received")    
         
     while robot.step(TIME_STEP) != -1 and not rospy.is_shutdown():
-
+        lateral_car_1.setVelocity([0,-5,0, 0,0,0])
+        lateral_car_2.setVelocity([0,-6,0, 0,0,0])
         i = 0 
         for car in cars:
             if car is not None:        
